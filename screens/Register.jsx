d440523b7b17fe2313c95f1351deb0fc79442494
ikowirya <ikowirya@gmail.com>
2024-02-29 21:57:51 +0700

    update

--- screens/Register.jsx
+++ screens/Register.jsx
@@ -15,10 +15,13 @@ import {
   INPUT_PASSWORD,
   INPUT_USERNAME,
 } from "../utils/Constants";
+import { isValidEmail, isValidPassword } from "../utils/Validation";
 
 const Register = ({ navigation }) => {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
+  const [validPassword, setValidPassword] = useState(true);
+  const [validUsername, setValidUsername] = useState(true);
   const auth = FIREBASE_AUTH;
   useEffect(() => {
     AsyncStorage.getItem("token").then((token) => {
@@ -28,22 +31,44 @@ const Register = ({ navigation }) => {
     });
   }, []);
 
+  {
+    /* function validate email */
+  }
+  const handleEmail = (input) => {
+    setUsername(input);
+    const data = isValidEmail(input);
+    data ? setValidUsername(true) : setValidUsername(false);
+  };
+
+  {
+    /* function validate password */
+  }
+  const handlePassword = (input) => {
+    setPassword(input);
+    const data = isValidPassword(input);
+    data ? setValidPassword(true) : setValidPassword(false);
+  };
+
   const handleRegister = () => {
-    createUserWithEmailAndPassword(auth, username, password)
-      .then((response) =>
-        response.user
-          .getIdToken()
-          .then((token) => AsyncStorage.setItem("token", token))
-          .then(() => {
-            Alert.alert(CREATED_SUCCESS, `Welcome ${response.user.email}`, [
-              {
-                text: "Ok",
-                onPress: () => navigation.navigate("home"),
-              },
-            ]);
-          })
-      )
-      .catch((error) => Alert.alert(error.message));
+    if (validPassword && validUsername) {
+      createUserWithEmailAndPassword(auth, username, password)
+        .then((response) =>
+          response.user
+            .getIdToken()
+            .then((token) => AsyncStorage.setItem("token", token))
+            .then(() => {
+              Alert.alert(CREATED_SUCCESS, `Welcome ${response.user.email}`, [
+                {
+                  text: "Ok",
+                  onPress: () => navigation.navigate("home"),
+                },
+              ]);
+            })
+        )
+        .catch((error) => Alert.alert(error.message));
+    } else {
+      Alert.alert(`${INPUT_VALIDATE}`);
+    }
   };
 
   return (
@@ -52,16 +77,23 @@ const Register = ({ navigation }) => {
         style={styles.input}
         placeholder={INPUT_USERNAME}
         value={username}
-        onChangeText={setUsername}
+        onChangeText={(text) => handleEmail(text)}
       />
+      {/* Message validate */}
+      {!validPassword && (
+        <Text style={styles.validation}>Password not valid</Text>
+      )}
       <TextInput
         style={styles.input}
         placeholder={INPUT_PASSWORD}
         value={password}
-        onChangeText={setPassword}
+        onChangeText={(text) => handlePassword(text)}
         secureTextEntry
       />
-
+      {/* Message validate */}
+      {!validPassword && (
+        <Text style={styles.validation}>Password not valid</Text>
+      )}
       <TouchableOpacity style={styles.register} onPress={handleRegister}>
         <Text>Register</Text>
       </TouchableOpacity>
@@ -71,15 +103,14 @@ const Register = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
+    justifyContent: "top",
   },
   input: {
     marginHorizontal: 10,
     padding: 20,
     marginVertical: 10,
     borderWidth: 1,
-    borderRadius: 10
+    borderRadius: 10,
   },
   register: {
     padding: 10,
@@ -87,7 +118,12 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     backgroundColor: "grey",
     alignItems: "center",
-    borderRadius: 10
+    borderRadius: 10,
+  },
+  validation: {
+    color: "red",
+    marginHorizontal: 10,
+    marginBottom: 10,
   },
 });
 export default Register;
