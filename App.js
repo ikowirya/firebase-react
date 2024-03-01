6cf547ba87a7f8eecdb81438c3d2967bf467dde1
ikowirya <ikowirya@gmail.com>
2024-03-01 09:02:06 +0700

    update

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./screens/Login";
 import Home from "./screens/Home";
 import Register from "./screens/Register";
+import { Provider } from "react-redux";
+import configureStore from "./reducers";
+import Detail from "./screens/Detail";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="login">
-        <Stack.Screen name="login" component={Login}   options={{title: 'Login'}}></Stack.Screen>
-        <Stack.Screen name="register" component={Register}   options={{title: 'Register'}}></Stack.Screen>
-        <Stack.Screen name="home" component={Home}   options={{title: 'Home', headerShown: false}}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={configureStore}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="login">
+          <Stack.Screen
+            name="login"
+            component={Login}
+            options={{ title: "Login" }}
+          />
+          <Stack.Screen
+            name="register"
+            component={Register}
+            options={{ title: "Register" }}
+          />
+          <Stack.Screen
+            name="home"
+            component={Home}
+            options={{ title: "Home", headerShown: false }}
+          />
+           <Stack.Screen
+            name="detail"
+            component={Detail}
+            options={{ title: "Detail" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -2,16 +2,22 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect, useState } from "react";
 import { View, StyleSheet, Text, Button, FlatList, Image } from "react-native";
 import { BASE_URL, REJECT_AUTH } from "../utils/Constants";
+import { useDispatch, useSelector } from "react-redux";
+import { counterActions } from "../reducers/counter";
 
 const Home = ({ navigation }) => {
   const [anime, setAnime] = useState([]);
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
   
-  {/* function logout */}
+  /* function logout */
   const handleLogout = () => {
     AsyncStorage.removeItem("token").then(() => navigation.navigate("login"));
   };
+  /* function logout */
 
-  {/* function check token */}
+  /* function check token */
+  /*
   useEffect(() => {
     AsyncStorage.getItem("token")
       .then((token) => {
@@ -23,39 +29,49 @@ const Home = ({ navigation }) => {
       .then((response) => response.json())
       .then(({ data }) => setAnime(data));
   }, []);
-
-  {/* UI item list */}
-  const renderAnime = ({ item }) => (
+  */
+  /* function check token */
+  
+  /* UI item list */
+  /* const renderAnime = ({ item }) => (
     <View style={styles.sectionList}>
       <Image
         source={{ uri: item.images.jpg.image_url, width: 64, height: 64 }}
       />
       <Text style={styles.title}>{item.title}</Text>
     </View>
-  );
+  ); */
+   /* UI item list */
 
   return (
     <View style={styles.container}>
+      <Text>{count}</Text>
+      <Button title="Increment" onPress={()=> dispatch(counterActions.increment())}/>
+      <Button title="Decrement" onPress={()=> dispatch(counterActions.decrement())}/>
+      <Button title="Go to Detail" onPress={()=> navigation.navigate("detail")}/>
       <Button title="Logout" onPress={handleLogout} />
-      <FlatList data={anime} renderItem={renderAnime} />
+      {/* <FlatList data={anime} renderItem={renderAnime} /> */}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex:1,
     marginTop: 45,
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
-    marginLeft:10, 
-    fontWeight:"bold", 
-    flex:1, 
-    marginRight: 24
+    marginLeft: 10,
+    fontWeight: "bold",
+    flex: 1,
+    marginRight: 24,
   },
   sectionList: {
-    flexDirection: "row", 
-    paddingLeft:10, 
-    marginBottom:10
-  }
+    flexDirection: "row",
+    paddingLeft: 10,
+    marginBottom: 10,
+  },
 });
 export default Home;
